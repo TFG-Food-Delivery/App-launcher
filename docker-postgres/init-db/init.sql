43f4e95113ac66dd5daa520847598ef2a15c4a2d
Emanuel Peña <emanuel.cuevas03@gmail.com>
2025-01-13 19:29:23 +0100

    chore: update environment variables and add new microservices to docker-compose

---
+++ docker-postgres/init-db/init.sql
@@ -0,0 +1,7 @@
+-- init.sql
+-- Script para crear las bases de datos requeridas
+CREATE DATABASE authdb;
+CREATE DATABASE restaurantdb;
+CREATE DATABASE courierdb;
+CREATE DATABASE customerdb;
+CREATE DATABASE orderdb;
